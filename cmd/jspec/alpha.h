212581bf63074adcc2d37f12a13f53b23899edb2
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-08-23 02:32:42 -0400

    Add JSpec .alpha identity header

---
+++ cmd/jspec/alpha.h
@@ -0,0 +1,12 @@
+#ifndef JSPEC_ALPHA_H
+#define JSPEC_ALPHA_H
+
+#define JSPEC_ALPHA_MAGIC "ALPHA-ELF-JSPEC-1"
+#define JSPEC_ALPHA_VERSION 1
+
+/*
+ * Public identity constants for the JSpec .alpha executable family.
+ * Linux v1 uses ELF as the actual OS-recognized binary format.
+ */
+
+#endif
