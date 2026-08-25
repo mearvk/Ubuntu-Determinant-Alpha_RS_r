77fc295c180472aecdbd34765ca7aab9dc9104d6
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-08-25 16:04:02 -0400

    Finalize XMC ASYSMA OS registration interface

--- tools/xmc/xmc-os-register.h
+++ tools/xmc/xmc-os-register.h
@@ -1,7 +1,15 @@
 #ifndef XMC_OS_REGISTER_H
 #define XMC_OS_REGISTER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int xmc_register_asysma(const char *desktop_file, const char *program_name,
                         const char *icon_path, const char *executable_path);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
