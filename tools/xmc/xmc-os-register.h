5681df25794593b927f69e9e1d0e2e4b2b312609
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-08-25 15:58:56 -0400

    Add XMC OS registration declaration

---
+++ tools/xmc/xmc-os-register.h
@@ -0,0 +1,7 @@
+#ifndef XMC_OS_REGISTER_H
+#define XMC_OS_REGISTER_H
+
+int xmc_register_asysma(const char *desktop_file, const char *program_name,
+                        const char *icon_path, const char *executable_path);
+
+#endif
