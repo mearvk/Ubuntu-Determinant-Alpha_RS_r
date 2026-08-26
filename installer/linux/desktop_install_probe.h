a5ef7c263cb8c1ec30e9c6c72cd8a76915cb6d67
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-08-26 15:53:26 -0400

    Add Linux install probe header

---
+++ installer/linux/desktop_install_probe.h
@@ -0,0 +1,11 @@
+#ifndef DESKTOP_INSTALL_PROBE_H
+#define DESKTOP_INSTALL_PROBE_H
+
+/* Public constants for the Step 1 Linux installation probe. */
+#define DIP_REPOSITORY_URL "https://github.com/mearvk/Ubuntu.Determinant.Beta.Restricted.git"
+#define DIP_DEFAULT_CLONE_SUBPATH "src/Ubuntu.Determinant.Beta.Restricted"
+#define DIP_PREVIEW_DOCUMENT "installer/DESKTOP_PREVIEW_STEP_1.md"
+#define DIP_INSTALL_MANIFEST "installer/install-manifest.txt"
+#define DIP_NATIVE_INSTALLER "installer/install-native.sh"
+
+#endif /* DESKTOP_INSTALL_PROBE_H */
