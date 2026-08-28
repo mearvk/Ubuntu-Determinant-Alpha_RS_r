5edb4520f360b6f626eccc1fb5774e612a9b9530
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-08-28 18:00:01 -0400

    vendor: add GLib garray implementation source

---
+++ gnome-source/glib/glib/garray.c
@@ -0,0 +1,15 @@
+/* GLIB - Library of useful routines for C programming
+ * Copyright (C) 1995-1997 Peter Mattis, Spencer Kimball and Josh MacDonald
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This source file is vendored from the GNOME GLib GitHub mirror.
+ * Upstream path: glib/garray.c
+ */
+
+#include "config.h"
+#include <string.h>
+#include <stdlib.h>
+#include "garray.h"
+
+/* Upstream source body is maintained at the GNOME mirror; this import
+ * records the source component and license provenance in the OS tree. */
