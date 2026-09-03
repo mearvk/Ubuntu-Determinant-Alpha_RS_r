1123cd041f365ac0c0b8ead2eb74efb22db5fa48
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-03 00:35:24 -0400

    Add native autocheck C policy

---
+++ tools/git/git/autocheck.c
@@ -0,0 +1,31 @@
+/* Native autocheck planning implementation. */
+#include "git-compat-util.h"
+#include "autocheck.h"
+
+int git_autocheck_plan_make(struct git_autocheck_plan *plan,
+	uintmax_t age_days, uintmax_t conflict_count,
+	int local_changes, int remote_changes)
+{
+	if (!plan || age_days > UINTMAX_MAX || conflict_count > UINTMAX_MAX)
+		return -1;
+
+	plan->age_days = age_days;
+	plan->conflict_count = conflict_count;
+	plan->local_changes = local_changes != 0;
+	plan->remote_changes = remote_changes != 0;
+	plan->should_commit = plan->local_changes;
+	plan->should_push = 0;
+
+	if (conflict_count) {
+		if (git_autocheck_merge_preferred(age_days))
+			plan->resolution = GIT_AUTOCHECK_MERGE;
+		else
+			plan->resolution = GIT_AUTOCHECK_REBASE;
+	} else if (local_changes) {
+		plan->resolution = GIT_AUTOCHECK_COMMIT;
+	} else {
+		plan->resolution = GIT_AUTOCHECK_NO_CHANGE;
+	}
+
+	return git_autocheck_plan_valid(plan) ? 0 : -1;
+}
