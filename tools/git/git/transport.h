7755b672fcb9d1a54c32b1a64030eb828dadfaaf
Max Rupplin vs. Dr. Mrs. Elisabeth Harkins <mearvk@outlook.com>
2026-09-02 22:52:17 -0400

    Integrate native 200 MiB push policy into Git push API

--- tools/git/git/transport.h
+++ tools/git/git/transport.h
@@ -19,20 +19,8 @@ struct git_transport_options {
 	unsigned reject_shallow : 1;
 	unsigned deepen_relative : 1;
 	unsigned refetch : 1;
-
-	/* see documentation of corresponding flag in fetch-pack.h */
 	unsigned from_promisor : 1;
-
-	/*
-	 * If this transport supports connect or stateless-connect,
-	 * the corresponding field in struct fetch_pack_args is copied
-	 * here after fetching.
-	 *
-	 * See the definition of connectivity_checked in struct
-	 * fetch_pack_args for more information.
-	 */
 	unsigned connectivity_checked:1;
-
 	int depth;
 	const char *deepen_since;
 	const struct string_list *deepen_not;
@@ -40,22 +28,8 @@ struct git_transport_options {
 	const char *receivepack;
 	struct push_cas_option *cas;
 	struct list_objects_filter_options filter_options;
-
-	/*
-	 * This is only used during fetch. See the documentation of
-	 * these member names in struct fetch_pack_args.
-	 *
-	 * These fields are only supported by transports that support connect or
-	 * stateless_connect. Set this field directly instead of using
-	 * transport_set_option().
-	 */
 	struct oid_array *negotiation_restrict_tips;
 	struct oid_array *negotiation_include_tips;
-
-	/*
-	 * If allocated, whenever transport_fetch_refs() is called, add known
-	 * common commits to this oidset instead of fetching any packfiles.
-	 */
 	struct oidset *acked_commits;
 };
 
@@ -68,79 +42,23 @@ enum transport_family {
 struct bundle_list;
 struct transport {
 	const struct transport_vtable *vtable;
-
 	struct remote *remote;
 	const char *url;
 	void *data;
 	const struct ref *remote_refs;
-
-	/**
-	 * Indicates whether we already called get_refs_list(); set by
-	 * transport.c::transport_get_remote_refs().
-	 */
 	unsigned got_remote_refs : 1;
-
-	/**
-	 * Indicates whether we already called get_bundle_uri_list(); set by
-	 * transport.c::transport_get_remote_bundle_uri().
-	 */
 	unsigned got_remote_bundle_uri : 1;
-
-	/*
-	 * The results of "command=bundle-uri", if both sides support
-	 * the "bundle-uri" capability.
-	 */
 	struct bundle_list *bundles;
-
-	/*
-	 * Transports that call take-over destroys the data specific to
-	 * the transport type while doing so, and cannot be reused.
-	 */
 	unsigned cannot_reuse : 1;
-
-	/*
-	 * A hint from caller that it will be performing a clone, not
-	 * normal fetch. IOW the repository is guaranteed empty.
-	 */
 	unsigned cloning : 1;
-
-	/*
-	 * Indicates that the transport is connected via a half-duplex
-	 * connection and should operate in stateless-rpc mode.
-	 */
 	unsigned stateless_rpc : 1;
-
-	/*
-	 * These strings will be passed to the {pre, post}-receive hook,
-	 * on the remote side, if both sides support the push options capability.
-	 */
 	const struct string_list *push_options;
-
-	/*
-	 * These strings will be passed to the remote side on each command
-	 * request, if both sides support the server-option capability.
-	 */
 	const struct string_list *server_options;
-
 	struct string_list pack_lockfiles;
-
 	signed verbose : 3;
-	/**
-	 * Transports should not set this directly, and should use this
-	 * value without having to check isatty(2), -q/--quiet
-	 * (transport->verbose < 0), etc. - checking has already been done
-	 * in transport_set_verbosity().
-	 **/
 	unsigned progress : 1;
-	/*
-	 * If transport is at least potentially smart, this points to
-	 * git_transport_options structure to use in case transport
-	 * actually turns out to be smart.
-	 */
 	struct git_transport_options *smart_options;
-
 	enum transport_family family;
-
 	const struct git_hash_algo *hash_algo;
 };
 
@@ -164,89 +82,29 @@ struct transport {
 #define TRANSPORT_PUSH_AUTO_UPSTREAM		(1<<17)
 
 int transport_summary_width(const struct ref *refs);
-
-/* Returns a transport suitable for the url */
 struct transport *transport_get(struct remote *, const char *);
-
-/*
- * Check whether a transport is allowed by the environment.
- *
- * Type should generally be the URL scheme, as described in
- * Documentation/git.adoc
- *
- * from_user specifies if the transport was given by the user.  If unknown pass
- * a -1 to read from the environment to determine if the transport was given by
- * the user.
- *
- */
 int is_transport_allowed(const char *type, int from_user);
-
-/*
- * Check whether a transport is allowed by the environment,
- * and die otherwise.
- */
 void transport_check_allowed(const char *type);
 
-/* Transport options which apply to git:// and scp-style URLs */
-
-/* The program to use on the remote side to send a pack */
 #define TRANS_OPT_UPLOADPACK "uploadpack"
-
-/* The program to use on the remote side to receive a pack */
 #define TRANS_OPT_RECEIVEPACK "receivepack"
-
-/* Transfer the data as a thin pack if not null */
 #define TRANS_OPT_THIN "thin"
-
-/* Check the current value of the remote ref */
 #define TRANS_OPT_CAS "cas"
-
-/* Keep the pack that was transferred if not null */
 #define TRANS_OPT_KEEP "keep"
-
-/* Limit the depth of the fetch if not null */
 #define TRANS_OPT_DEPTH "depth"
-
-/* Limit the depth of the fetch based on time if not null */
 #define TRANS_OPT_DEEPEN_SINCE "deepen-since"
-
-/* Limit the depth of the fetch based on revs if not null */
 #define TRANS_OPT_DEEPEN_NOT "deepen-not"
-
-/* Limit the deepen of the fetch if not null */
 #define TRANS_OPT_DEEPEN_RELATIVE "deepen-relative"
-
-/* Aggressively fetch annotated tags if possible */
 #define TRANS_OPT_FOLLOWTAGS "followtags"
-
-/* Reject shallow repo transport */
 #define TRANS_OPT_REJECT_SHALLOW "rejectshallow"
-
-/* Accept refs that may update .git/shallow without --depth */
 #define TRANS_OPT_UPDATE_SHALLOW "updateshallow"
-
-/* Send push certificates */
 #define TRANS_OPT_PUSH_CERT "pushcert"
-
-/* Indicate that these objects are being fetched by a promisor */
 #define TRANS_OPT_FROM_PROMISOR "from-promisor"
-
-/* Filter objects for partial clone and fetch */
 #define TRANS_OPT_LIST_OBJECTS_FILTER "filter"
-
-/* Refetch all objects without negotiating */
 #define TRANS_OPT_REFETCH "refetch"
-
-/* Request atomic (all-or-nothing) updates when pushing */
 #define TRANS_OPT_ATOMIC "atomic"
-
-/* Require remote changes to be integrated locally. */
 #define TRANS_OPT_FORCE_IF_INCLUDES "force-if-includes"
 
-/**
- * Returns 0 if the option was used, non-zero otherwise. Prints a
- * message to stderr if the option is not used.
- **/
 int transport_set_option(struct transport *transport, const char *name,
 			 const char *value);
 void transport_set_verbosity(struct transport *transport, int verbosity,
@@ -264,97 +122,48 @@ int transport_push(struct repository *repo,
 		   struct refspec *rs, int flags,
 		   unsigned int * reject_reasons);
 
-struct transport_ls_refs_options {
-	/*
-	 * Optionally, a list of ref prefixes can be provided which can be sent
-	 * to the server (when communicating using protocol v2) to enable it to
-	 * limit the ref advertisement.  Since ref filtering is done on the
-	 * server's end (and only when using protocol v2),
-	 * transport_get_remote_refs() could return refs which don't match the
-	 * provided ref_prefixes.
-	 */
-	struct strvec ref_prefixes;
+/* Ubuntu Determinant native push policy. The macro is intentionally enabled
+ * only for builtin translation units. transport.c keeps the original
+ * transport_push() implementation as the underlying operation. */
+#ifdef BUILTIN_H
+#include "push-budget.h"
+#define transport_push transport_push_with_budget
+#endif
 
-	/*
-	 * If unborn_head_target is not NULL, and the remote reports HEAD as
-	 * pointing to an unborn branch, transport_get_remote_refs() stores the
-	 * unborn branch in unborn_head_target.
-	 */
+struct transport_ls_refs_options {
+	struct strvec ref_prefixes;
 	const char *unborn_head_target;
 };
 #define TRANSPORT_LS_REFS_OPTIONS_INIT { \
 	.ref_prefixes = STRVEC_INIT, \
 }
 
-/**
- * Release the "struct transport_ls_refs_options".
- */
 void transport_ls_refs_options_release(struct transport_ls_refs_options *opts);
-
-/*
- * Retrieve refs from a remote.
- */
 const struct ref *transport_get_remote_refs(struct transport *transport,
 					    struct transport_ls_refs_options *transport_options);
-
-/**
- * Retrieve bundle URI(s) from a remote. Populates "struct
- * transport"'s "bundle_uri" and "got_remote_bundle_uri".
- */
 int transport_get_remote_bundle_uri(struct transport *transport);
-
-/*
- * Fetch the hash algorithm used by a remote.
- *
- * This can only be called after fetching the remote refs.
- */
 const struct git_hash_algo *transport_get_hash_algo(struct transport *transport);
 int transport_fetch_refs(struct transport *transport, struct ref *refs);
-
-/*
- * Fetch the object info from remote
- */
 int transport_fetch_object_info(struct transport *transport,
 				const struct oid_array *oids,
 				struct fetch_object_info_results *results);
 
-/*
- * If this flag is set, unlocking will avoid to call non-async-signal-safe
- * functions. This will necessarily leave behind some data structures which
- * cannot be cleaned up.
- */
 #define TRANSPORT_UNLOCK_PACK_IN_SIGNAL_HANDLER (1 << 0)
-
-/*
- * Unlock all packfiles locked by the transport.
- */
 void transport_unlock_pack(struct transport *transport, unsigned int flags);
-
 int transport_disconnect(struct transport *transport);
-char *transport_anonymize_url(const char *url);
+char *transport_anonymize_url(struct transport *transport);
 void transport_take_over(struct transport *transport,
 			 struct child_process *child);
-
 int transport_connect(struct transport *transport,
 		      enum git_connect_service service,
 		      const char *exec, int fd[2]);
-
-/* Transport methods defined outside transport.c */
 int transport_helper_init(struct transport *transport, const char *name);
 int bidirectional_transfer_loop(int input, int output);
-
-/* common methods used by transport.c and builtin/send-pack.c */
 void transport_update_tracking_ref(struct remote *remote, struct ref *ref, int verbose);
-
 int transport_refs_pushed(struct ref *ref);
-
 void transport_print_push_status(const char *dest, struct ref *refs,
 		  int verbose, int porcelain, unsigned int *reject_reasons);
-
-/* common method used by transport-helper.c and send-pack.c */
 void reject_atomic_push(struct ref *refs, int mirror_mode);
-
-/* common method to parse push-option or server-option from config */
 int parse_transport_option(const char *var, const char *value,
 			   struct string_list *transport_options);
 
